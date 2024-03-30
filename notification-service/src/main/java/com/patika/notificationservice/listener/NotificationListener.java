6548b8065992646bedbccb17f379ec6ac7bba5f3
Hakan AFAT <phantom3ee@gmail.com>
2024-03-31 00:41:18 +0300

    Add strategy pattern to notification service

--- notification-service/src/main/java/com/patika/notificationservice/listener/NotificationListener.java
+++ notification-service/src/main/java/com/patika/notificationservice/listener/NotificationListener.java
@@ -1,6 +1,11 @@
 package com.patika.notificationservice.listener;
 
 import com.patika.notificationservice.dto.NotificationDTO;
+import com.patika.notificationservice.operations.EmailOperation;
+import com.patika.notificationservice.operations.MobileOperation;
+import com.patika.notificationservice.operations.SmsOperation;
+import com.patika.notificationservice.operations.TransferOperation;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -13,6 +18,26 @@ public class NotificationListener {
     public void sendNotification(NotificationDTO notificationDTO) {
 
         log.info("kuyruktan okudun: {}", notificationDTO);
+
+        TransferOperation myop = null;
+
+        switch (notificationDTO.getNotificationType()) {
+            case EMAIL:
+                myop = new TransferOperation(new EmailOperation());
+                break;
+            case SMS:
+                myop = new TransferOperation(new SmsOperation());
+                break;
+            case MOBILE_NOTIFICATION:
+                myop = new TransferOperation(new MobileOperation());
+                break;
+            default:
+                break;
+        }
+
+        if(myop != null) {
+            myop.Send(notificationDTO.getMessage()); 
+        } 
 
     }
 
