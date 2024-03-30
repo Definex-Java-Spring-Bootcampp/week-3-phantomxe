380b04af13fd324b4df7f6fcbb4bd2ffa6b3185d
Hakan AFAT <phantom3ee@gmail.com>
2024-03-30 15:59:06 +0300

    Add exception logging for kredinbizde service

--- kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/exceptions/GlobalExceptionHandler.java
+++ kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/exceptions/GlobalExceptionHandler.java
@@ -1,19 +1,35 @@
 package com.patika.kredinbizdeservice.exceptions;
 
+import com.patika.kredinbizdeservice.dto.KafkaLog;
 import com.patika.kredinbizdeservice.exceptions.dto.ExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+ 
+    @Autowired
+    private KafkaTemplate<Object, Object> kafkaTemplate; 
 
     @ExceptionHandler(KredinbizdeException.class)
     public ResponseEntity<ExceptionResponse> handleKredinbizdeException(KredinbizdeException exception) {
         log.error("exception occurred. {0}", exception.getCause());
+ 
+        //send exceptions to kafka
+        kafkaTemplate.send("patika.logs", new KafkaLog("exception-kredinbizde-service", ExceptionUtils.getFullStackTrace(exception) ));
+        log.info("Exception sent to Kafka");
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
@@ -23,6 +39,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleAllException(Exception exception) {
         log.error("exception occurred. {0}", exception.getCause());
+
+        //print stack trace 
+
+        //send exceptions to kafka
+
+        kafkaTemplate.send("patika.logs", new KafkaLog("exception-kredinbizde-service", ExceptionUtils.getFullStackTrace(exception) ));
+        log.info("Exception sent to Kafka");
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
