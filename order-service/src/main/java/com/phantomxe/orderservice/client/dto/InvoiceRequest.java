1e9ac090642b4a9dfe57cac57540fa67371deecd
Hakan AFAT <phantom3ee@gmail.com>
2024-04-01 23:35:44 +0300

    fix serialization and path variable bug

--- order-service/src/main/java/com/phantomxe/orderservice/client/dto/InvoiceRequest.java
+++ order-service/src/main/java/com/phantomxe/orderservice/client/dto/InvoiceRequest.java
@@ -6,12 +6,18 @@ import com.phantomxe.orderservice.entity.Customer;
 import com.phantomxe.orderservice.entity.Product;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@Builder
 public class InvoiceRequest {
     private Customer customer;
     private List<Product> products;
--- order-service/src/main/java/com/phantomxe/orderservice/client/dto/InvoiceResponse.java
+++ order-service/src/main/java/com/phantomxe/orderservice/client/dto/InvoiceResponse.java
@@ -1,12 +1,18 @@
 package com.phantomxe.orderservice.client.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@Builder
 public class InvoiceResponse {
     private Integer invoiceId;
 }
